b92fe3105f499afb82a60e97b8ec0740926f1b15
Joseph Puma <josephpuma@Josephs-MacBook-Air.local>
2022-06-14 10:04:45 -0500

    second commit

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const { Sequelize } = require('sequelize');
 
 const hostname = process.env.LOCALHOST_DATABASE.replace(/['"]+/g, '')
 
+console.log(hostname);
+
 const sequelize = new Sequelize(process.env.NAME_DATABASE, process.env.USER_DATABASE, process.env.PASSWORD_DATABASE, {
         host: hostname,
         dialect: 'mysql'
